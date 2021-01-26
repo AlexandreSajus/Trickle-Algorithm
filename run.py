203606094f6fd65c6f2d4e925bd7c197d0d9fa1c
SELVESTREL Alexandre <alexandre.selvestrel@student-cs.fr>
2021-01-26 17:52:07 +0100

    dynamique tentative 1

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from random import random, randint
+import numpy as np
 
 
 class Node:
-    def __init__(self, id_number, i_min, i_max, k, i, tau, c, messages, ld, md):
+    def __init__(self, id_number, i_min, i_max, k, i, tau, c, messages, ld, md, t=0):
         self.id_number = id_number  # identifies the node, Int
         self.i_min = i_min  # minimum of interval I
         self.i_max = i_max  # maximum of interval I
@@ -14,6 +15,7 @@ class Node:
         self.messages = messages
         self.ld = ld  # LD, contains the current fragments of software
         self.md = md  # MD, contains the current number of version
+        self.t = t
 
     def check_version(self, message, k):
         """
@@ -74,6 +76,68 @@ class Node:
                 self.i = self.i_min
                 self.c = 0
 
+    def act_2(self, apres_tau):
+       # check if any version received in messages is different from the current one
+        version_change = True
+
+        while len(self.messages) > 0:
+            # check the messages, if the current version is lower, update; if it is higher, send it to neighbouring nodes
+            message = self.messages.pop()
+            for k in range(n_fragments):
+                if self.check_version(message, k) == 0:
+                    self.c += 1
+                elif self.check_version(message, k) == -1:
+                    version_change = False
+                    self.send_message()
+                elif self.check_version(message, k) == 1:
+                    version_change = False
+                    self.ld[k] = message[1][k]
+                    self.md[k] = message[2][k]
+
+        if self.c < self.k and apres_tau == False:
+            # if c < k, send our version to neighbouring
+            self.send_message()
+
+        if apres_tau == True:
+            if version_change == True:
+                # if a version was different, extend i
+                self.i = self.i*2
+            else:
+                # if not, reset i and c
+                self.i = self.i_min
+                self.c = 0
+
+
+def tourne(nodes, T_max):
+    T_tot = 0
+    while T_tot < T_max:
+        avant_tau = []
+        avant_i = []
+        for node in nodes:
+            duree_tau = node.tau - node.t
+            duree_i = node.i - node.t
+            if duree_tau >= 0:
+                avant_tau.append([node, duree_tau])
+            else:
+                avant_i.append([node, duree_i])
+        arg_min = None
+        duree_min = np.inf
+        apres_tau = False
+        for elem in avant_tau:
+            if elem[1] < duree_min:
+                duree_min = elem[1]
+                arg_min = elem[0]
+        for elem in avant_i:
+            if elem[1] < duree_min:
+                duree_min = elem[1]
+                arg_min = elem[0]
+                apres_tau = True
+        for node in nodes:
+            node.t = node.t + duree_min
+        T_tot = T_tot + duree_min
+        arg_min.act_2(apres_tau)
+    return nodes
+
 
 nodes = [Node(0, 0, 10, 2, 5, 100, 0, [], [True, True], [1, 1]),
          Node(1, 0, 10, 2, 5, 100, 0, [], [False, False], [2, 2])]
@@ -88,10 +152,15 @@ print(nodes[1].md)
 
 
 if __name__ == "__main__":
-    A = Node(0, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_2", "code_fragment_2_version_2"], [True, True])
-    B = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    C = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    D = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    E = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    A = Node(0, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [],
+             ["code_fragment_1_version_2", "code_fragment_2_version_2"], [True, True])
+    B = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    C = Node(2, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    D = Node(3, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    E = Node(4, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
     neighbors = {0: [1, 3], 1: [2, 4], 2: [4], 3: [4], 4: [0, 1]}
-    
+    tourne(nodes, 80)

20bbda5f3a7ce06df096f40011330da9f847377d
Alexandre Sajus <asajus47@gmail.com>
2021-01-27 08:47:36 +0100

    Updated run with act_2

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from random import random, randint
+from random import random, randint, uniform
+import numpy as np
 
 
 class Node:
-    def __init__(self, id_number, i_min, i_max, k, i, tau, c, messages, ld, md):
+    def __init__(self, id_number, i_min, i_max, k, i, tau, c, messages, ld, md, t=0):
         self.id_number = id_number  # identifies the node, Int
         self.i_min = i_min  # minimum of interval I
         self.i_max = i_max  # maximum of interval I
@@ -14,6 +15,7 @@ class Node:
         self.messages = messages
         self.ld = ld  # LD, contains the current fragments of software
         self.md = md  # MD, contains the current number of version
+        self.t = t
 
     def check_version(self, message, k):
         """
@@ -35,7 +37,7 @@ class Node:
     def send_message(self):
         # Sends a message with the current version to neighbouring nodes
         for recipient in neighbors[self.id_number]:
-            recipient.messages.append(self.id_number, self.ld, self.md)
+            recipient.messages.append([self.id_number, self.ld, self.md])
 
     def act(self, t):
         # Acts according to the time t
@@ -74,24 +76,99 @@ class Node:
                 self.i = self.i_min
                 self.c = 0
 
+    def act_2(self, apres_tau):
+       # check if any version received in messages is different from the current one
+        version_change = True
 
-nodes = [Node(0, 0, 10, 2, 5, 100, 0, [], [True, True], [1, 1]),
-         Node(1, 0, 10, 2, 5, 100, 0, [], [False, False], [2, 2])]
+        # le noeud a reçu ses messages AVANT de regarder ce qu'il fait.
+        while len(self.messages) > 0:
+            # check the messages, if the current version is lower, update; if it is higher, send it to neighbouring nodes
+            message = self.messages.pop()
+            for k in range(n_fragments):
+                if self.check_version(message, k) == 0:
+                    self.c += 1
+                elif self.check_version(message, k) == -1:
+                    version_change = False
+                    self.send_message()
+                elif self.check_version(message, k) == 1:
+                    version_change = False
+                    self.ld[k] = message[1][k]
+                    self.md[k] = message[2][k]
 
-t = 0
-n_nodes = 2
-n_fragments = 2
-neighbors = {0: [1], 1: [0]}
+        if self.c < self.k and apres_tau == False:
+            # if c < k, send our version to neighbouring
+            self.send_message()
 
-nodes[1].act(t)
-print(nodes[1].md)
+        if apres_tau == True:
+            if version_change == True:
+                # if a version was different, extend i
+                self.i = self.i*2
+            else:
+                # if not, reset i and c
+                self.i = self.i_min
+                self.c = 0
+            self.tau = uniform(self.i/2, self.i)
+            self.t = 0
+
+
+def tourne(nodes, T_max):
+    '''Fait tourner le réseau pendant T_max'''
+    T_tot = 0  # Durée totale de l'expérience
+    non_tau = []  # à ne pas refaire agire, initialiser
+    non_i = []  # à ne pas refaire agire, initialiser
+    duree_min = np.inf  # durée minimale avant la prochaine action, initialiser
+    while T_tot < T_max:
+        avant_tau = []
+        avant_i = []
+        for node in nodes:
+            duree_tau = node.tau - node.t
+            duree_i = node.i - node.t
+            if duree_tau >= 0:
+                avant_tau.append([node, duree_tau])
+            avant_i.append([node, duree_i])
+        arg_min = None
+        duree_min = np.inf
+        apres_tau = False
+        for elem in avant_tau:  # trouver le noeud qui va agir en prochain. Est-ce en arrivant à tau?
+            if elem[1] < duree_min:
+                if not elem[0] in non_tau:
+                    duree_min = elem[1]
+                    arg_min = elem[0]
+        for elem in avant_i:  # trouver le noeud qui va agir en prochain. Est-ce en arrivant à i?
+            if elem[1] < duree_min:
+                if not elem[0] in non_i:
+                    duree_min = elem[1]
+                    arg_min = elem[0]
+                    apres_tau = True
+        if duree_min > 0:  # Si on a bougé en temps depuis la dernière fois, on peut refaire agir les noeuds ayant agi la dernière fois
+            non_tau = []
+            non_i = []
+        for node in nodes:
+            node.t = node.t + duree_min  # le temps avance de durée_min
+        T_tot = T_tot + duree_min  # le temps avance de durée min
+        arg_min.act_2(apres_tau)  # on fait agir le noeud qui arrive à échéance
+        if apres_tau == False:
+            # ne pas refaire agir ce noeud tant qu'on a pas changé d'instant
+            non_tau.append(arg_min)
+        else:
+            # ne pas refaire agir ce noeud tant qu'on a pas changé d'instant
+            non_i.append(arg_min)
+    return nodes
 
 
 if __name__ == "__main__":
-    A = Node(0, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_2", "code_fragment_2_version_2"], [True, True])
-    B = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    C = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    D = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    E = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1, 0, [], ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
-    neighbors = {0: [1, 3], 1: [2, 4], 2: [4], 3: [4], 4: [0, 1]}
-    
+    A = Node(0, 1, 2, randint(1, 3), 1, random()*1/2 + 1/2, 0, [],
+             ["code_fragment_1_version_2", "code_fragment_2_version_2"], [True, True])
+    B = Node(1, 1, 2, randint(1, 3), 1, random()*1/2 + 1/2, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    C = Node(2, 1, 2, randint(1, 3), 1, random()*1/2 + 1/2, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    D = Node(3, 1, 2, randint(1, 3), 1, random()*1/2 + 1/2, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    E = Node(4, 1, 2, randint(1, 3), 1, random()*1/2 + 1/2, 0, [],
+             ["code_fragment_1_version_1", "code_fragment_2_version_1"], [False, False])
+    neighbors = {0: [B, D], 1: [D, E], 2: [E], 3: [E, C], 4: [A, B]}
+    nodes = [A, B, C, D, E]
+    n_fragments = 2
+    tourne(nodes, 80)
+    print([A.md, B.md, C.md, D.md, E.md])
